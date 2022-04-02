51ca7fbc0121f1dd9329f2870e7b4e9ad165e46f
git939 <98944155+git939@users.noreply.github.com>
2022-04-02 14:07:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,8 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-//(function () {
+
+(function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
@@ -61,8 +62,7 @@ for (var i = 0; i < names.length; i++); {
     helloSpeaker.speak(names[i]);
   } else {
     byeSpeaker.speak(names[i]);
-   
   }
 }
 
-}})();
+})();
